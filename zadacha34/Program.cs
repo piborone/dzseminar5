3862aad53648156bc2773370a1aff5c740bf80d8
Pavel B <boronenkov999@mail.ru>
2023-01-31 00:09:43 +0300

    метод переписан через while

--- zadacha34/Program.cs
+++ zadacha34/Program.cs
@@ -3,43 +3,25 @@
 // [345, 897, 568, 234] -> 2
 int Razmer(int numbers) // определение положительной длинны массива
 {
-    if(numbers > 0)
+        while(numbers < 1)
     {
-        
+        System.Console.WriteLine("Введеное значение не является положительным");
+        System.Console.WriteLine("Введите необходимую длинну массива повторно: ");
+        numbers = Convert.ToInt32(Console.ReadLine());
     }
-    System.Console.WriteLine("Введеное значение не является положительным");// не прекращает работ у выводит пустой массив
-      
+
 return numbers;
 }
 
-int InputNumbers(string str)
-{
-    int numbers;
-    string? text;
-    while(true)
-    {
-        Console.WriteLine(str);
-        text = Console.ReadLine();
-        if(int.TryParse(text, out numbers))
-        {
-            break;
-        }
-        Console.WriteLine("Ошибка!!! Введено не верное значение!");
-    }
-    return numbers;
-}
-
-//Console.WriteLine("Введите необходимую длинну массива: ");
-int razmer = InputNumbers("Введите необходимую длинну массива: ");
+Console.WriteLine("Введите необходимую длинну массива: ");
+int razmer = Convert.ToInt32(Console.ReadLine());
 int razmerprov = Razmer(razmer);
 int chet = 0;
-//if(razmer > 0)
-//{
     int[] array = new int[razmerprov];
     for(int i = 0; i < razmerprov; i++)
     {
     array[i] = new Random().Next(100, 1000);
-        if(array[i]%2 == 0)
+        if(array[i] % 2 == 0)
         {
          chet++;
         }
@@ -47,8 +29,3 @@ int chet = 0;
     System.Console.WriteLine("случайный массив");
     Console.WriteLine($"[{String.Join("; ", array)}]");
     System.Console.WriteLine("кол-во четных элементов " + chet);
-//}
-//else
-//{
-//    System.Console.WriteLine("Введеное значение не является положительным");
-//}
